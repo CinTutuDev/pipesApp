1a3781d6c037579439cb32237427805efcc39f75
cinta <cintatafur@gmail.com>
2023-11-26 17:51:33 +0100

    ✏️ Decimal, Currency y Percent Pipes

--- src/app/products/pages/numbers-pages/numbers-pages.component.ts
+++ src/app/products/pages/numbers-pages/numbers-pages.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numbers-pages',
   templateUrl: './numbers-pages.component.html',
-  styleUrls: ['./numbers-pages.component.scss']
+  styleUrls: ['./numbers-pages.component.scss'],
 })
 export class NumbersPagesComponent {
-
+  public totalSell: number = 122664.2365;
+  public percent: number = 0.4856;//49%
 }
