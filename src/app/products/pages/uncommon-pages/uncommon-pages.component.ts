d72d71d7d11aa9edf8e869e84af682ea91abda27
cinta <cintatafur@gmail.com>
2023-11-27 16:19:34 +0100

    📌 SlicePipe

--- src/app/products/pages/uncommon-pages/uncommon-pages.component.ts
+++ src/app/products/pages/uncommon-pages/uncommon-pages.component.ts
@@ -3,8 +3,42 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-pages',
   templateUrl: './uncommon-pages.component.html',
-  styleUrls: ['./uncommon-pages.component.scss']
+  styleUrls: ['./uncommon-pages.component.scss'],
 })
 export class UncommonPagesComponent {
+  //i18n Select
+  public name: string = 'CinTutuDev';
+  public gender: 'male' | 'female' = 'female';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 
+  changeClient(): void {
+    this.name = 'Daniel';
+    this.gender = 'male';
+  }
+
+  //i18nPlural
+
+  public clients: string[] = [
+    'Daienl',
+    'CCristina',
+    'Pepe',
+    'Lola',
+    'Agustin',
+    'Jeusus',
+    'Maria',
+    'Soledad',
+    'Cinat',
+  ];
+  public clientsMap = {
+    '=0': ' No tenemos ningún cliente esperando',
+    '=1': ' tenemos un cliente esperando',
+    '=2': ' tenemos dos cliente esperando',
+    other: ' tenemos # clientes esperando.',
+  };
+  deletClient(): void {
+    this.clients.shift();
+  }
 }
